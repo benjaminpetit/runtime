1c7c86092d20b747d30b5bbceab6806b79dffd68
Benjamin Petit <bpetit@microsoft.com>
2023-05-17 20:15:24 +0200

    WIP

--- src/libraries/Microsoft.Extensions.DependencyInjection/tests/DI.Tests/KeyedServiceProviderContainerTests.cs
+++ src/libraries/Microsoft.Extensions.DependencyInjection/tests/DI.Tests/KeyedServiceProviderContainerTests.cs
@@ -27,13 +27,85 @@ namespace Microsoft.Extensions.DependencyInjection.Tests
             Assert.Same(service2, provider.GetKeyedService<IService>("service2"));
         }
 
+        [Fact]
+        public void ResolveKeyedServiceSingletonInstance()
+        {
+            var service = new Service();
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddKeyedSingleton<IService>("service1", service);
+
+            var provider = CreateServiceProvider(serviceCollection);
+
+            Assert.Null(provider.GetService<IService>());
+            Assert.Same(service, provider.GetKeyedService<IService>("service1"));
+        }
+
+        [Fact]
+        public void ResolveKeyedServiceSingletonFactory()
+        {
+            var service = new Service();
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddKeyedSingleton<IService>("service1", sp => service);
+
+            var provider = CreateServiceProvider(serviceCollection);
+
+            Assert.Null(provider.GetService<IService>());
+            Assert.Same(service, provider.GetKeyedService<IService>("service1"));
+        }
+
+        [Fact]
+        public void ResolveKeyedServiceSingletonType()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddKeyedSingleton<IService, Service>("service1");
+
+            var provider = CreateServiceProvider(serviceCollection);
+
+            Assert.Null(provider.GetService<IService>());
+            Assert.Equal(typeof(Service), provider.GetKeyedService<IService>("service1")!.GetType());
+        }
+
+        [Fact]
+        public void ResolveKeyedServiceTransientFactory()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddKeyedTransient<IService>("service1", (sp, key) => new Service(key as string));
+
+            var provider = CreateServiceProvider(serviceCollection);
+
+            Assert.Null(provider.GetService<IService>());
+            var first = provider.GetKeyedService<IService>("service1");
+            var second = provider.GetKeyedService<IService>("service1");
+            Assert.NotSame(first, second);
+            Assert.Equal("service1", first.ToString());
+            Assert.Equal("service1", second.ToString());
+        }
+
+        [Fact]
+        public void ResolveKeyedServiceTransientType()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddKeyedTransient<IService, Service>("service1");
+
+            var provider = CreateServiceProvider(serviceCollection);
+
+            Assert.Null(provider.GetService<IService>());
+            var first = provider.GetKeyedService<IService>("service1");
+            var second = provider.GetKeyedService<IService>("service1");
+            Assert.NotSame(first, second);
+        }
+
         interface IService { }
 
         class Service : IService
         {
-            private readonly Guid _id = Guid.NewGuid();
+            private readonly string _id;
 
-            public override string? ToString() => _id.ToString();
+            public Service() => _id = Guid.NewGuid().ToString();
+
+            public Service(string id) => _id = id;
+
+            public override string? ToString() => _id;
         }
     }
 }
